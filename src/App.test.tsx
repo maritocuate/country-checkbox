6c1ac94d2f80d401bc539e154e54637205ea50bb
maritocuate <mario300@gmail.com>
2024-03-04 12:00:57 -0300

    ✨ app test setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,43 @@
 import App from './App'
+import { render, fireEvent, screen } from '@testing-library/react'
 
-test('Should allow individual selection of countries', () => {
-  expect(App(1, 2)).toBe(3)
+describe('App', () => {
+  let allCheckbox: HTMLInputElement
+  let indiaCheckbox: HTMLInputElement
+  let usaCheckbox: HTMLInputElement
+  let franceCheckbox: HTMLInputElement
+
+  beforeEach(() => {
+    render(<App />)
+
+    allCheckbox = screen.getByLabelText('all')
+    indiaCheckbox = screen.getByTestId('india')
+    usaCheckbox = screen.getByTestId('usa')
+    franceCheckbox = screen.getByTestId('france')
+  })
+
+  it('should allow individual selection of countries', async () => {
+    fireEvent.click(indiaCheckbox)
+    expect(indiaCheckbox.checked).toBe(true)
+  })
+
+  it('should select all checkboxes when "Select All" is clicked', async () => {
+    fireEvent.click(allCheckbox)
+    expect(indiaCheckbox.checked).toBe(true)
+    expect(usaCheckbox.checked).toBe(true)
+    expect(franceCheckbox.checked).toBe(true)
+  })
+
+  it('should deselect "Select All" when a country is deselected', async () => {
+    fireEvent.click(allCheckbox)
+    fireEvent.click(indiaCheckbox)
+    expect(allCheckbox.checked).toBe(false)
+  })
+
+  it('should select "Select All" when all countries are selected', async () => {
+    fireEvent.click(indiaCheckbox)
+    fireEvent.click(usaCheckbox)
+    fireEvent.click(franceCheckbox)
+    expect(allCheckbox.checked).toBe(true)
+  })
 })
