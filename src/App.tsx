3d2d6f80ce0c5dca5e898bac923b9579fd189e12
maritocuate <mario300@gmail.com>
2024-03-04 12:30:00 -0300

    ✨ app ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Checkbox } from '@/components/ui/checkbox'
+
 import './App.css'
+import { Card } from './components/ui/card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-5">Country Check</h1>
+
+      <div className="w-full md:w-1/2 lg:w-1/3">
+        <Card className="flex flex-col p-5 text-start">
+          <label className="space-x-2 flex items-center">
+            <Checkbox id="all" name="all" />
+            <span>
+              <strong>Select All</strong>
+            </span>
+          </label>
+          <label className="space-x-2 flex items-center">
+            <Checkbox id="india" />
+            <span>India</span>
+          </label>
+          <label className="space-x-2 flex items-center">
+            <Checkbox id="usa" />
+            <span>USA</span>
+          </label>
+          <label className="space-x-2 flex items-center">
+            <Checkbox id="france" />
+            <span>France</span>
+          </label>
+        </Card>
       </div>
-    </>
+    </main>
   )
 }
 
